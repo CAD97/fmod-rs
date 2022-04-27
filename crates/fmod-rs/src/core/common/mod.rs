e9316efad4e95a830f68302abf7a4010826b7a1a
CAD97 <cad97@cad97.com>
2022-04-27 08:19:06 -0500

    More system functionality

--- crates/fmod-rs/src/core/common/mod.rs
+++ crates/fmod-rs/src/core/common/mod.rs
@@ -3,3 +3,15 @@ mod flags;
 mod structs;
 
 pub use self::{enums::*, flags::*, structs::*};
+
+use fmod::raw::*;
+
+/// Maximum number of channels per frame of audio supported by audio files,
+/// buffers, connections and DSPs.
+pub const MAX_CHANNEL_WIDTH: i32 = FMOD_MAX_CHANNEL_WIDTH as i32;
+
+/// Maximum number of listeners supported.
+pub const MAX_LISTENERS: i32 = FMOD_MAX_LISTENERS as i32;
+
+/// Maximum number of System objects allowed.
+pub const MAX_SYSTEMS: i32 = FMOD_MAX_SYSTEMS as i32;
