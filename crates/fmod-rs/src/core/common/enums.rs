458b9a4f27c21c385eedfcb28aba3bac799a1026
Christopher Durham <cad97@cad97.com>
2022-06-05 17:11:45 -0400

    Add fmod_ios header

--- crates/fmod-rs/src/core/common/enums.rs
+++ crates/fmod-rs/src/core/common/enums.rs
@@ -660,4 +660,22 @@ enum_struct! {
         Vibration      = FMOD_PORT_TYPE_VIBRATION,
         Aux            = FMOD_PORT_TYPE_AUX,
     }
+
+    #[cfg_attr(feature = "unstable", doc(cfg(target_os = "ios")))]
+    /// Control whether the sound will use a the dedicated hardware decoder or a
+    /// software codec.
+    ///
+    /// Every devices has a single hardware decoder and unlimited software
+    /// decoders.
+    pub enum AudioQueueCodecPolicy: i32 {
+        /// Try hardware first, if it's in use or prohibited by audio session,
+        /// try software.
+        Default      = FMOD_AUDIOQUEUE_CODECPOLICY_DEFAULT,
+        /// `kAudioQueueHardwareCodecPolicy_UseSoftwareOnly` ~ try software,
+        /// if not available fail.
+        SoftwareOnly = FMOD_AUDIOQUEUE_CODECPOLICY_SOFTWAREONLY,
+        /// `kAudioQueueHardwareCodecPolicy_UseHardwareOnly` ~ try hardware,
+        /// if not available fail.
+        HardwareOnly = FMOD_AUDIOQUEUE_CODECPOLICY_HARDWAREONLY,
+    }
 }
